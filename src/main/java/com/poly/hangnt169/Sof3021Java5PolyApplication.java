c1210437bdedd576560176acedc89b98c383cd0d
HangNT169 <HangNT169@fpt.edu.vn>
2023-02-27 15:39:07 +0700

    Update

--- src/main/java/com/poly/hangnt169/Sof3021Java5PolyApplication.java
+++ src/main/java/com/poly/hangnt169/Sof3021Java5PolyApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Sof3021Java5PolyBasicApplication {
+public class Sof3021Java5PolyApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Sof3021Java5PolyBasicApplication.class, args);
+        SpringApplication.run(Sof3021Java5PolyApplication.class, args);
     }
 
 }
